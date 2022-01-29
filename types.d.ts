a5fcb313555f8b10711e1aeff7bb160b9db44153
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-01-29 15:46:31 -0500

    Initial commit

--- types.d.ts
+++ types.d.ts
@@ -1,1 +1,21 @@
 import {BeDecoratedProps} from 'be-decorated/types';
+
+export interface BeBasedVirtualProps{
+    rules?: BeBasedRule[]
+}
+
+export interface BeBasedProps extends BeBasedVirtualProps{
+    proxy:  Element & BeBasedVirtualProps,
+}
+
+export interface BeBasedRule {
+    selector: string,
+    attr: string,
+    ifNot: string,
+    prependVal: string,
+}
+
+export interface BeBasedActions{
+
+}
+
