c647b2db6cc6740435765934e2a2e962a7b1e84e
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-05-12 06:25:22 -0400

    0.0.61

--- behivior.js
+++ behivior.js
@@ -2,6 +2,7 @@ import './behance.js';
 import { BeHive } from 'be-hive/be-hive.js';
 BeHive.registry.register({
     base: 'be-based',
+    enhPropKey: 'beBased',
     map: {
         '0.0': 'base'
     },
--- behivior.ts
+++ behivior.ts
@@ -4,6 +4,7 @@ import {BeHive} from 'be-hive/be-hive.js';
 
 BeHive.registry.register({
     base: 'be-based',
+    enhPropKey: 'beBased',
     map: {
         '0.0': 'base'
     },
