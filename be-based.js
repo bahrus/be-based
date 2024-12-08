cddb7f96df3fee5a8fd1da68c1387d5d3e726506
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-07 20:06:50 -0500

    0.0.71

--- be-based.js
+++ be-based.js
@@ -2,7 +2,7 @@
 import { config as beCnfg } from 'be-enhanced/config.js';
 import { BE } from 'be-enhanced/BE.js';
 /** @import {BEConfig, IEnhancement,  BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
-/** @import {Actions, PAP, AllProps, AP} from './types.d.ts' */;
+/** @import {Actions, PAP, AllProps, AP} from './ts-refs/be-based/types.d.ts' */;
 import { MountObserver } from 'mount-observer/MountObserver.js';
 
 /**
